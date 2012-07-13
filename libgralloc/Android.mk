c49a54bdcb252f525557fa19460cb85ee532059e
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2012-07-13 14:47:54 +0100

    Fix building libmemalloc without ION
    Conflicts:
    	libgralloc/Android.mk

--- libgralloc/Android.mk
+++ libgralloc/Android.mk
@@ -30,10 +30,23 @@ include $(BUILD_SHARED_LIBRARY)
 
 #MemAlloc Library
 include $(CLEAR_VARS)
-LOCAL_MODULE           := libmemalloc
-LOCAL_MODULE_TAGS      := optional
-LOCAL_C_INCLUDES       := $(common_includes)
-LOCAL_SHARED_LIBRARIES := $(common_libs) libgenlock libqdutils
-LOCAL_CFLAGS           := $(common_flags) -DLOG_TAG=\"memalloc\"
-LOCAL_SRC_FILES        :=  ionalloc.cpp alloc_controller.cpp
+LOCAL_PRELINK_MODULE := false
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
+LOCAL_C_INCLUDES := hardware/qcom/display/libqdutils
+LOCAL_SHARED_LIBRARIES := liblog libcutils libutils
+LOCAL_SRC_FILES :=  alloc_controller.cpp
+LOCAL_CFLAGS:= -DLOG_TAG=\"memalloc\" -DLOG_NDDEBUG=0
+
+ifeq ($(TARGET_USES_ION),true)
+    LOCAL_SRC_FILES += ionalloc.cpp
+    LOCAL_CFLAGS += -DUSE_ION
+else
+    LOCAL_SRC_FILES += ashmemalloc.cpp \
+                    pmemalloc.cpp \
+                    pmem_bestfit_alloc.cpp
+endif
+
+LOCAL_CFLAGS:= -DLOG_TAG=\"memalloc\"
+LOCAL_MODULE := libmemalloc
+LOCAL_MODULE_TAGS := optional
 include $(BUILD_SHARED_LIBRARY)
